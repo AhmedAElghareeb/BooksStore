a3f184ad9e9117aec2d7e95b1515f6e754ac3050
Ahmed Elghareeb <ahmed.elghareeb1166@gmail.com>
2024-01-11 17:52:42 +0200

    create home repo methods & failure method...

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:books_app/core/errors/failures.dart';
+import 'package:books_app/features/home/data/models/books_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo
 {
-
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
